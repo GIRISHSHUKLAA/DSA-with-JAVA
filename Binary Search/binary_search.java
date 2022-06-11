2bf5584e8d14370fe2a90ab69bf38a51a5f8572c
GIRISHSHUKLAA <93051418+GIRISHSHUKLAA@users.noreply.github.com>
2022-06-12 01:01:34 +0530

    Update binary_search.java

--- Binary Search/binary_search.java
+++ Binary Search/binary_search.java
@@ -1,7 +1,8 @@
 /*
  * the code is valid for only if array is sorted in ascending order
  * and to apply code u must know wheather arry is sorted in ascending order or descending order
- * in that case we use Order-Agnostic binary search
+ * 
+ * if we do not know whear=ther array is sorted in ascending or descending order than we use Order-Agnostic Binary Search
  */
 
 
