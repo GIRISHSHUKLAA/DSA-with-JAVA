2a8679d175b3e9e3d16f6cf0a299f6577a7d5d44
GIRISHSHUKLAA <93051418+GIRISHSHUKLAA@users.noreply.github.com>
2022-08-16 00:53:47 +0530

    Binary search using recursion

---
+++ RECURSION/BS.java
@@ -0,0 +1,19 @@
+public class BS {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 55, 66, 78};
+        System.out.println(search(arr,4,0,arr.length-1));
+    }
+    static int search(int[] arr, int target, int s, int e) {
+        if (s > e) {
+            return -1;
+        }
+        int m = s + (e - s) / 2;
+        if (arr[m] == target) {
+            return m;
+        }
+        if (target < arr[m]) {
+            return search(arr, target, s, m - 1);
+        }
+        return search(arr, target, m + 1, e);
+    }
+}
