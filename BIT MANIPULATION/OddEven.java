1939a7b5288fccb80955965566af0ee22568b3ce
GIRISHSHUKLAA <93051418+GIRISHSHUKLAA@users.noreply.github.com>
2022-09-04 20:27:21 +0530

    Given a number n find it is odd or even

--- BIT MANIPULATION/OddEven.java
+++ BIT MANIPULATION/OddEven.java
@@ -6,5 +6,6 @@ public class OddEven {
 
     private static boolean isEven(int n) {
         return (n & 1) != 1;
+//         if (n & 1)==1 than number is odd if (n & 1)==0 than number is even 
     }
 }
