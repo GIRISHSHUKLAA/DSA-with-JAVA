6b1957cfa7855096c03dfef83bb58d14f3ab3fcc
GIRISHSHUKLAA <93051418+GIRISHSHUKLAA@users.noreply.github.com>
2022-09-06 19:21:06 +0530

    Program to check square root of a number Binar

---
+++ MATHS/perfectSquareRoot.java
@@ -0,0 +1,23 @@
+public class perfectSquareRoot {
+    public static void main(String[] args) {
+        int n = 40;
+        int s = 0;
+        int e = n;
+        int ans = 0;
+        while (s < e) {
+            int mid = s + (e - s) / 2;
+            if (mid * mid == n) {
+                ans = mid;
+                break;
+            } else if (mid * mid > n) {
+                e = mid - 1;
+            } else {
+                s = mid + 1;
+            }
+        }
+        if (ans == 0)
+            System.out.println("Not a perfect square");
+        else
+            System.out.println(ans);
+    }
+}
