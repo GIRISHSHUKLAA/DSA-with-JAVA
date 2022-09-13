ceac8726f8f857e8220867e9b9f5c0fd6615bf2f
GIRISHSHUKLAA <93051418+GIRISHSHUKLAA@users.noreply.github.com>
2022-09-13 23:17:48 +0530

    Print the sum of n number

---
+++ RECURSION/Basic Questions/sum.java
@@ -0,0 +1,15 @@
+public class sum {
+    public static void main(String[] args) {
+        int n = sum(5);
+        System.out.println(n);
+    }
+
+    static int sum(int n) {
+        if (n == 1)
+            return 1;
+        else {
+
+            return n + sum(n - 1);
+        }
+    }
+}
