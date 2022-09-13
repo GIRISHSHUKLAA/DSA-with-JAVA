9d1449af0cd3e3334c93ad7ec3dfebf481683c88
GIRISHSHUKLAA <93051418+GIRISHSHUKLAA@users.noreply.github.com>
2022-09-13 23:39:50 +0530

    Find the sum of digit of a number

---
+++ RECURSION/Basic Questions/sumOfDigit.java
@@ -0,0 +1,14 @@
+public class sum {
+    public static void main(String[] args) {
+        int n = sum(1342);
+        System.out.println(n);
+    }
+
+    static int sum(int n) {
+        if (n == 0)
+            return 0;
+        else {
+            return n % 10 + sum(n / 10);
+        }
+    }
+}
