470bbaa5adb674096556613ecc380e68ce2cb7d5
GIRISHSHUKLAA <93051418+GIRISHSHUKLAA@users.noreply.github.com>
2022-09-25 23:26:54 +0530

    Create linearSearch.java

---
+++ RECURSION/Basic Questions/linearSearch.java
@@ -0,0 +1,25 @@
+public class linearSearch {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 38, 4, 5, 6};
+        System.out.println(search(arr, 38, 0));
+        System.out.println(index(arr, 38, 0));
+
+    }
+
+    private static boolean search(int[] arr, int target, int index) {
+        if (index == arr.length)
+            return false;
+        return arr[index] == target || search(arr, target, index + 1);
+    }
+
+    // if you want to return index value
+    private static int index(int[] arr, int target, int index) {
+        if (index == arr.length)
+            return -1;
+        if (arr[index] == target)
+            return index;
+
+        else
+            return index(arr, target, index + 1);
+    }
+}
