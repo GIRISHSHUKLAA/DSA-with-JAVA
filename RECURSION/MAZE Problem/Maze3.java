8471adca3132e8ea020aaa9e4555d4bd0326ffe5
GIRISHSHUKLAA <93051418+GIRISHSHUKLAA@users.noreply.github.com>
2022-10-04 02:08:32 +0530

    Include diagonal also
    Same as Maze2 only difference is here we can go diagonally also.

---
+++ RECURSION/MAZE Problem/Maze3.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class Maze3 {
+    public static void main(String[] args) {
+        System.out.println(pathRetDiag("", 3, 3));
+    }
+
+
+    static ArrayList<String> pathRetDiag(String ans, int r, int c) {
+        if (r == 1 && c == 1) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(ans);
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>();
+        if (r > 1) {
+            list.addAll(pathRetDiag(ans + "D", r - 1, c-1));
+        }
+
+        if (r > 1) {
+            list.addAll(pathRetDiag(ans + "V", r - 1, c));
+        }
+        if (c > 1) {
+            list.addAll(pathRetDiag(ans + "H", r, c - 1));
+        }
+        return list;
+
+    }
+}
