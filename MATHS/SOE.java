2787e2762c9e150e3e8f6a982026d13bc9e96b9c
GIRISHSHUKLAA <93051418+GIRISHSHUKLAA@users.noreply.github.com>
2022-10-10 21:50:20 +0530

    Check prime number (Sieve of Eratosthenes)

--- MATHS/SOE.java
+++ MATHS/SOE.java
@@ -1,36 +1,26 @@
+import java.util.Arrays;
+
 public class prime {
     public static void main(String[] args) {
-        int n1 = 40;
-        for (int i = 2; i <= n1; i++) {
-            System.out.println(i + " " + isPrime(i));
-        }
-//       optimized way:-Sieve of Eratosthenes
-//       time complexity:- O(N*log(log N))
         int n = 40;
-        boolean[] primes = new boolean[n + 1];
-        sieve(n, primes);
+        printPrime(n);
+        System.out.println();
+        sieve(n);
+        System.out.println();
+        sieve1(n);
     }
 
-//    initially all the elements in an array are false
-//    False in array means number is prime
-    private static void sieve(int n, boolean[] primes) {
-        for (int i = 2; i * i <= n; i++) {
-            if (!primes[i]) {
-//              if a number is prime means its multiple is not prime so make it true
-                for (int j = 2 * i; j <= n; j+=i) {
-//                     you can further optimized this by replacing j-2*i with i*i
-                    primes[j] = true;
-                }
-            }
-        }
-        for (int i = 2; i < n; i++) {
-            if (!primes[i]){
-                System.out.print(i+" ");
+    static void printPrime(int n) {
+//        Time Complexity:- O(n * root n)
+//        n for this loop and root n for isPrime
+        for (int i = 2; i <= n; i++) {
+            if (prime(i)) {
+                System.out.print(i + " ");
             }
         }
     }
 
-    static boolean isPrime(int n) {
+    static boolean prime(int n) {
         if (n <= 1) {
             return false;
         }
@@ -42,5 +32,56 @@ public class prime {
             c++;
         }
         return true;
+    }
+
+
+
+    /*
+     *                           Optimized way:-Sieve of Eratosthenes
+     * Idea:- Create a boolean array of size [n+1]
+     * We initialize this array as true at the end the entries which are prime will remain true and else become false
+     * if a number is prime means its multiple is not prime so make it false
+     *
+     */
+
+    static void sieve(int n) {
+//        Simple implementation of sieve algorithm
+//        Time complexity:- O(N*log(log N))
+        if (n <= 1) {
+            return;
+        }
+
+        boolean isPrime[] = new boolean[n + 1];
+
+        Arrays.fill(isPrime, true);
+
+        for (int i = 2; i * i <= n; i++) {
+            if (isPrime[i]) {
+                for (int j = 2 * i; j <= n; j = j + i) {
+//                    Instead of 2i you can also use i*i
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        for (int i = 2; i <= n; i++) {
+            if (isPrime[i])
+                System.out.print(i + " ");
+        }
+    }
+
+//    shorter implementation of optimized sieve
+
+    static void sieve1(int n) {
+        boolean isPrime[] = new boolean[n + 1];
+        Arrays.fill(isPrime, true);
+        for (int i = 2; i <= n; i++) {
+            if (isPrime[i]) {
+                System.out.print(i + " ");
+                for (int j = i * i; j <= n; j = j + i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
     }
 }
