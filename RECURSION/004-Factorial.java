a733299fab8571ccb82f97243baa84a6977a8890
GIRISHSHUKLAA <93051418+GIRISHSHUKLAA@users.noreply.github.com>
2022-10-16 02:01:50 +0530

    Find the factorial of a number

--- RECURSION/004-Factorial.java
+++ RECURSION/004-Factorial.java
@@ -3,13 +3,23 @@ public class recursion {
         int n = factorial(10);
         System.out.println(n);
     }
-
+// Not a tail recursive 
     static int factorial(int n) {
         if (n == 1)
             return 1;
         else {
 
             return n * factorial(n - 1);
+        }
+    }
+    
+//     TAIL RECURSIVE 
+    
+        static int factorial2(int n, int k) {
+        if (n == 1)
+            return k;
+        else {
+            return factorial2(n - 1, n * k);
         }
     }
 }
