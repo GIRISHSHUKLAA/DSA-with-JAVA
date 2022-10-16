4d897cd1b3ca2c9448974e24bb174f770f587a96
GIRISHSHUKLAA <93051418+GIRISHSHUKLAA@users.noreply.github.com>
2022-10-17 01:14:11 +0530

    Pattern

---
+++ RECURSION/017-Triangle1.java
@@ -0,0 +1,24 @@
+// * * * *
+// * * *
+// * *
+// *
+
+public class pattern {
+    public static void main(String[] args) {
+       triangle(4,0);
+    }
+
+    static void triangle(int r, int c) {
+        if (r == 0)
+            return;
+        if (c < r) {
+            System.out.print("* ");
+            triangle(r, c + 1);
+        }
+        else{
+            System.out.println();
+            triangle(r-1,0);
+        }
+    }
+
+}
