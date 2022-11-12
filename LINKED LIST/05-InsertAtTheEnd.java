513588b6efb143fbcd039a5e1e055d426b5a6de4
GIRISHSHUKLAA <93051418+GIRISHSHUKLAA@users.noreply.github.com>
2022-11-12 11:32:15 +0530

    Insert at the end of Singly Linked List

---
+++ LINKED LIST/05-InsertAtTheEnd.java
@@ -0,0 +1,42 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+
+public class InsertAtBeginSinglyLinkedList {
+    public static void main(String[] args) {
+        Node head = null;
+        head = insertAtEnd(head, 10);
+        head = insertAtEnd(head, 20);
+        head = insertAtEnd(head, 30);
+        printList(head);
+    }
+
+    private static void printList(Node head) {
+        Node curr = head;
+        while (curr != null) {
+            System.out.println(curr.data);
+            curr = curr.next;
+        }
+    }
+
+    public static Node insertAtEnd(Node head, int x) {
+//        Time Complexity:- O(1)
+        Node temp = new Node(x);
+        if (head == null) {
+            return temp;
+        }
+        Node curr = head;
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+        curr.next = temp;
+        return head;
+    }
+}
