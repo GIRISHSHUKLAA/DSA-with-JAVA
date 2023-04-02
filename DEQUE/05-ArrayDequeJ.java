18fe5389045540b20288a929cd8f1cf625e14d01
GIRISHSHUKLAA <93051418+GIRISHSHUKLAA@users.noreply.github.com>
2023-04-02 20:52:34 +0530

    ArrayDeque in Java

---
+++ DEQUE/05-ArrayDequeJ.java
@@ -0,0 +1,11 @@
+import java.util.ArrayDeque;
+
+public class ArrayDequeJ {
+    public static void main(String[] args) {
+        ArrayDeque<Integer> ad = new ArrayDeque<>();
+        ad.add(10);
+        ad.add(20);
+        ad.add(30);
+        System.out.println(ad);
+    }
+}
