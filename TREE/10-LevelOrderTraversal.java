a9775e9fc8eae40eb3e8747ba78d47c0edd5e97a
GIRISHSHUKLAA <93051418+GIRISHSHUKLAA@users.noreply.github.com>
2023-04-22 16:58:23 +0530

    Level Order Traversal Line by Line (Part 2)

--- TREE/10-LevelOrderTraversal.java
+++ TREE/10-LevelOrderTraversal.java
@@ -1,47 +1,51 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
 
-class Node {
-    int key;
-    Node left;
-    Node right;
-
-    Node(int k) {
-        key = k;
-        left = right = null;
-    }
+class Node  
+{ 
+  int key; 
+  Node left; 
+  Node right; 
+  Node(int k){
+      key=k;
+      left=right=null;
+  }
 }
+
 
 public class LOT {
     //    Level Order Traversal Line By Line
-    public static void main(String[] args) {
-        Node root = new Node(10);
-        root.left = new Node(20);
-        root.right = new Node(30);
-        root.left.left = new Node(40);
-        root.left.right = new Node(50);
-        root.right.left = new Node(60);
-        root.right.right = new Node(70);
+    public static void main(String args[]) 
+    { 
+        Node root=new Node(10);
+    	root.left=new Node(20);
+    	root.right=new Node(30);
+    	root.left.left=new Node(40);
+    	root.left.right=new Node(50);
+    	root.right.left=new Node(60);
+    	root.right.right=new Node(70);
+    	
         printLevel(root);
-    }
-
-    public static void printLevel(Node root) {
+    } 
+    
+    public static void printLevel(Node root){
 //        Time Complexity:- O(n)
 //        Auxiliary Space:- O(n)
-        if (root == null) return;
-        Queue<Node> q = new LinkedList<>();
+        if(root==null)return;
+        Queue<Node> q=new LinkedList<>();
         q.add(root);
-        q.add(null);
-        while (q.size() > 1) {
-            Node curr = q.poll();
-            if (curr == null) {
-                System.out.println();
-                q.add(null);
-            } else {
-                System.out.print(curr.key + " ");
-                if (curr.left != null) q.add(curr.left);
-                if (curr.right != null) q.add(curr.right);
+        while(q.isEmpty()==false){
+            int count=q.size();
+            for(int i=0;i<count;i++){
+            Node curr=q.poll();
+            System.out.print(curr.key+" ");
+            if(curr.left!=null)
+                q.add(curr.left);
+            if(curr.right!=null)
+                q.add(curr.right);
             }
+            System.out.println();
         }
-    }
-}
+    }   
+} 
