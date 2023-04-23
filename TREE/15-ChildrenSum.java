627dbc88be394716f83b1f9d9796568e9b129171
GIRISHSHUKLAA <93051418+GIRISHSHUKLAA@users.noreply.github.com>
2023-04-23 22:35:44 +0530

    Children Sum Property
    Children Sum Property is a property in which the sum of values of the left child and right child should be equal to the value of their node if both children are present. Else if only one child is present then the value of the child should be equal to its node value.

---
+++ TREE/15-ChildrenSum.java
@@ -0,0 +1,37 @@
+class Node {
+    int key;
+    Node left;
+    Node right;
+
+    Node(int k) {
+        key = k;
+        left = right = null;
+    }
+}
+
+public class ChildrenSum {
+    public static void main(String[] args) {
+        Node root = new Node(20);
+        root.left = new Node(8);
+        root.right = new Node(12);
+        root.right.left = new Node(3);
+        root.right.right = new Node(9);
+
+        System.out.print(isCSum(root));
+
+    }
+
+    static boolean isCSum(Node root) {
+        if (root == null) return true;
+
+        if (root.left == null && root.right == null) return true;
+        
+        int sum = 0;
+        if (root.left != null) sum += root.left.key;
+        if (root.right != null) sum += root.right.key;
+
+        return (root.key == sum &&
+                isCSum(root.left) &&
+                isCSum(root.right));
+    }
+}
